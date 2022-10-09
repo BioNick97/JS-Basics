0b1da8d3084bf82e913ca5823fb191665770034a
Nick Hernandez <nicolas.hernandez144@Outlook.com>
2022-10-08 23:35:55 -0400

    10/08/2022

---
+++ objects.js
@@ -0,0 +1,15 @@
+let person = {
+  name: 'Nick',
+  age: 30
+};
+
+//dot notation (use)
+person.name = 'John';
+
+//bracket notation
+let selection = 'name';
+person['selection'] = 'mary';
+
+
+console.log(person.name);
+
